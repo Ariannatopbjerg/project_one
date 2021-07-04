5678ad1303e64b6d14663f4133b39b5a33b34622
Natalie Carr <77760936+NCarr2021@users.noreply.github.com>
2021-07-03 19:53:08 -0700

    update html, style

--- hsinyu/app.py
+++ hsinyu/app.py
@@ -1,5 +1,18 @@
-# for flask
 from flask import Flask, render_template
+import os
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func
+
+engine = create_engine(os.environ["DATABASE_AWS"])
+Base = automap_base()
+Base.prepare(engine, reflect=True)
+
+wildfire = Base.classes.wildfireDataFinal
+session=Session(engine)
+for row in session.query(wildfire).all():
+   print(row.State_name)
 
 app = Flask(__name__)
 
@@ -9,6 +22,4 @@ def index():
 
 if __name__ == "__main__":
    app.run()
-
-
 
